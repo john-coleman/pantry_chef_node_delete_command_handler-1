944389620fa6431c94b4dfcdf153c72889a1ec8c
Alex Slynko <alex.slynko@wonga.com>
2013-10-01 11:40:17 +0100

    first commit

---
+++ spec/pantry_chef_delete_node_command_handler/pantry_chef_delete_node_command_handler_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+require_relative '../../pantry_chef_delete_node_command_handler/pantry_chef_delete_node_command_handler'
+
+describe Wonga::Daemon::PantryChefDeleteNodeCommandHandler do
+  it_behaves_like 'handler'
+  describe "#handle_message" do
+    #add in unit test code for handle message here.
+  end
+end
+
